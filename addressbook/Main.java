02bda50c1833cb658a9460d0f7bfa13d7177a6b5
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-10-07 11:13:14 +0530

    Added:Functionality to add the new contact

--- addressbook/Main.java
+++ addressbook/Main.java
@@ -1,13 +1,32 @@
 package addressbook;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("welcome to address book program");
-		AddressBook person1 = new AddressBook("Kalyan","tanniru","12-1234","Guntur","AndhraPradesh","522616","9182169100","tannirukalyan1432@gmail.com");
-		AddressBook person2 = new AddressBook("Bhargav","tanniru","12-1234","Guntur","AndhraPradesh","522616","9642502811","tannirukalyan1234@gmail.com");
+		AddressBook person3 = new AddressBook();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter details of addressbook person3:");
+		System.out.println("Enter firtstname:");
+		person3.setFirstName(scanner.nextLine());
+		System.out.println("Enter lastname:");
+		person3.setLastName(scanner.nextLine());
+		System.out.println("Enter address:");
+		person3.setAddress(scanner.nextLine());
+		System.out.println("Enter city:");
+		person3.setCity(scanner.nextLine());
+		System.out.println("Enter state:");
+		person3.setState(scanner.nextLine());
+		System.out.println("Enter zip:");
+		person3.setZip(scanner.nextLine());
+		System.out.println("Enter phoneNumber:");
+		person3.setPhoneNumber(scanner.nextLine());
+		System.out.println("Enter email:");
+		person3.setEmail(scanner.nextLine());
+		scanner.close();
 		ContactBook contactBook = new ContactBook();
-		contactBook.add(person1);
-		contactBook.add(person2);
+		contactBook.add(person3);
 		UserInterface userInterface = new UserInterface();
 		userInterface.print(contactBook.getContactList());
 	}
